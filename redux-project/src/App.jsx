e5924b01d6ec9271d6af8be630fea4471106c95e
Abhishek Yadav <yabhishekk480@gmail.com>
2026-01-31 23:33:33 +0530

    Unsplash api

--- redux-project/src/App.jsx
+++ redux-project/src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { fetchPhotos } from "./api/mediaApi";
 
 const App = () => {
-  return  (
-  <>
-   <div className="h-screen w-full bg-gray-950 text-white" >
-    <h1>Hello</h1>
-  </div> </> 
+
+
+  return (
+    <>
+      <div className="h-screen w-full bg-gray-950 text-white" >
+        <h1>Hello</h1>
+        <button onClick={async() => {
+
+          const data= await fetchPhotos("car")
+          console.log(data)
+          
+        }
+        } > Get Photos </button>
+      </div> </>
   )
 };
 
-export default App; 
-  
+export default App;
---
+++ redux-project/src/api/mediaApi.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+const PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY;
+
+export async function fetchPhotos(query, page = 1, per_page = 20) {
+    const res = await axios.get('https://api.unsplash.com/search/photos', {
+        params: { query, page, per_page },
+        headers: {
+            Authorization: `Client-ID ${UNSPLASH_KEY}`
+        }
+    })
+    console.log(res);
+
+    return res.data;
+}
