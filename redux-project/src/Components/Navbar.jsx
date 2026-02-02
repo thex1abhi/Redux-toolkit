e4ba0983bfefb846508bba17ecd0ed087e583171
Abhishek Yadav <yabhishekk480@gmail.com>
2026-02-02 21:59:53 +0530

    Created navbar

---
+++ redux-project/src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <> 
+     <div className=" py-5 flex justify-between items-center   px-10 bg-(--c2)  " > <h2 className="font-medium text-2xl" > MediaSearch  </h2>
+      <div className="flex gap-5  items-center" >
+        <Link     className="text-base font-medium  active:scale-95  bg-(--c4) text-(--c1) rounded px-4 py-2   " to='/' >Search  </Link>
+        <Link  className="text-base font-medium  active:scale-95   bg-(--c4) text-(--c1) rounded px-4 py-2   "  to='/collection ' >Collection</Link>
+      </div>
+    </div>
+    </>
+  )
+};
+
+export default Navbar;
