d59b733d1299124caa2429023a34b1df5989d154
Abhishek Yadav <yabhishekk480@gmail.com>
2026-02-02 11:52:28 +0530

    Created home page

---
+++ redux-project/src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import SearchBar from "../Components/SearchBar";
+import Tabs from "../Components/Tabs";
+import ResultGrid from "../Components/ResultGrid";
+
+const Homepage = () => {
+  return <div>
+    <SearchBar />
+        <Tabs />
+        <ResultGrid />
+  </div>;
+};
+
+export default Homepage;
